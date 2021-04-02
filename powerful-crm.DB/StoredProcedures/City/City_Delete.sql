e706e0976c175c465b8b373ebc8d54d6c8b4c2ba
Zhanna <yellowcucumber27@gmail.com>
2021-04-02 23:00:36 +0300

    Added city procedures

---
+++ powerful-crm.DB/StoredProcedures/City/City_Delete.sql
@@ -0,0 +1,7 @@
+﻿create proc [dbo].[City_Delete]
+( @id int)
+as
+begin
+    DELETE from dbo.[City]
+    where Id=@id
+end
---
+++ powerful-crm.DB/StoredProcedures/City/dbo.City_Add.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[City_Add]
+	(@name nvarchar(100))
+AS
+begin
+	Insert into [dbo].[City]([Name])
+	values(@name)
+	select Scope_Identity()
+end
