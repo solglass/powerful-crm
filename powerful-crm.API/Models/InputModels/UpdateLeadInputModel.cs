7608259e55c0f2cd4b6246165faa3a6aa4c990b8
Oleg Lyamzin <o.s.like@yandex.ru>
2021-05-13 13:05:21 +0300

    CRM-17 Fix according with integration tests

--- powerful-crm.API/Models/InputModels/UpdateLeadInputModel.cs
+++ powerful-crm.API/Models/InputModels/UpdateLeadInputModel.cs
@@ -9,11 +9,11 @@ namespace powerful_crm.API.Models.InputModels
         [StringLength(100, MinimumLength = 2)]
         public string FirstName { get; set; }
         [Required]
-        [StringLength(100, MinimumLength = 2)]
+        [StringLength(100, MinimumLength = 3)]
         public string LastName { get; set; }
         [Required]
         [EmailAddress]
-        [StringLength(100)]
+        [StringLength(100, MinimumLength = 6)]
         public string Email { get; set; }
         [Required]
         [StringLength(20)]
@@ -22,7 +22,7 @@ namespace powerful_crm.API.Models.InputModels
         [CustomDateTimeValidation]
         public string BirthDate { get; set; }
         [Required]
-        [Range(0, int.MaxValue)]
+        [Range(1, int.MaxValue)]
         public int CityId { get; set; }
     }
 }
